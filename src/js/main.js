80f656b1729f6cf07ecc49a15f53f26de4b094fd
Maxine Athos <heyymax@icloud.com>
2026-02-19 02:14:28 -0300

    chore: refactor controller and slot UI (trainer-ready flow, input persistence)

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,24 @@ import { battleController } from "./controllers/battleController.js";
 
 arenaView.init({
   onBuscarPokemon: battleController.handleBuscarPokemon,
+  onTrocarPokemon: battleController.handleTrocarPokemon,
+  onTentarNovamente: battleController.handleTentarNovamente,
   onBatalhar: battleController.handleBatalhar,
+  onToggleReady: battleController.handleToggleReady,
 });
 
 modalView.init({
   onJogarNovamente: battleController.handleResetarDuelo,
 });
 
+// primeiro renderiza a estrutura estática (slots vazios)
+arenaView.renderShell();
+arenaView.renderBotaoBatalhar(false);
+
+// depois inicializa o controller que vai sincronizar o estado e preencher os slots
 battleController.init({
   onAtualizarUI: arenaView.render,
   onAbrirModal: modalView.open,
   onAtualizarBotao: arenaView.renderBotaoBatalhar,
-  onToast: arenaView.toast,
+  onAlerta: arenaView.alerta,
 });
-
-arenaView.renderShell();
-arenaView.renderBotaoBatalhar(false);
